2ace72247f3077e2eae2406da57b6ef89e0db95a
Deltik <deltik@gmx.com>
2018-11-15 22:21:47 -0600

    Configuration: Flexible configuration container
    e107\Configuration\ConfigurationInterface is a new standardized way to
    read and write configuration data. Data values are scalar and can be
    stored in a multidimensional array.
    
    The implementation is e107\Configuration\InMemoryConfiguration, which
    keeps configuration data in memory using standard PHP data types.
    
    Not yet implemented is
    e107\Configuration\Storage\ConfigurationStorageInterface, which is used
    to translate persistent storage of configuration data into a
    ConfigurationInterface object.
    
    A future commit should introduce an implementation of
    e107\Configuration\ConfigurationStorageInterface that translates the
    e107 v2 configuration file, e107_config.php, into a Configuration.
    
    e107\Configuration\Storage\AbstractFileConfigurationStorage handles
    reads and writes to one file in the file system.
    
    ----
    
    New Interfaces:
    * e107\Configuration\ConfigurationInterface
    * e107\Configuration\Storage\ConfigurationStorageInterface
    
    New Abstract Classes:
    * e107\Configuration\Storage\AbstractFileConfigurationStorage
    
    New Implementations:
    * e107\Configuration\InMemoryConfiguration
    
    New Exceptions:
    * e107\Configuration\Exceptions\ConfigurationException
    * e107\Configuration\Exceptions\ConfigurationKeyException
    * e107\Configuration\Exceptions\ConfigurationValueException

---
+++ e107_handlers/Configuration/Storage/AbstractFileConfigurationStorage.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * e107 website system
+ *
+ * Copyright (C) 2008-2018 e107 Inc (e107.org)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+ */
+
+namespace e107\Configuration\Storage;
+
+
+use e107\Configuration\ConfigurationInterface;
+
+/**
+ * Persistent storage that reads and writes Configuration data from and to a regular file
+ * @package e107\Configuration\Storage
+ */
+abstract class AbstractFileConfigurationStorage implements ConfigurationStorageInterface
+{
+	/** @var string */
+	protected $filePath;
+
+	/**
+	 * AbstractFileConfigurationStorage constructor
+	 * @param string $filePath
+	 *  Absolute path to the configuration file
+	 */
+	public function __construct($filePath)
+	{
+		$this->filePath = $filePath;
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function read()
+	{
+		$fileContents = file_get_contents($this->filePath);
+		if ($fileContents === FALSE)
+		{
+			throw new \RuntimeException(
+				"Could not read from file: {$this->filePath}"
+			);
+		}
+		return $this->fromFile($fileContents);
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function write($data)
+	{
+		$fileContents = $this->toFile($data);
+		$bytesWritten = file_put_contents($this->filePath, $fileContents);
+		if ($bytesWritten === FALSE)
+		{
+			throw new \RuntimeException(
+				"Could not write to file: {$this->filePath}"
+			);
+		}
+	}
+
+	/**
+	 * Convert the raw data from the configuration file
+	 * @param $data
+	 * @return string The parsed configuration data that can be imported by {@see ConfigurationInterface::populate()}
+	 */
+	abstract protected function fromFile($data);
+
+	/**
+	 * Convert the configuration data into the raw configuration file format
+	 * @param mixed $data The data from {@see ConfigurationInterface::save()}
+	 * @return string The exact contents to write into the configuration file
+	 */
+	abstract protected function toFile($data);
+}
